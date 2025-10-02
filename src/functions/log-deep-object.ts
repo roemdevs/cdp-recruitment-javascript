f456ceeb8850881a43a1da23515321e6e07b543d
Benjamin Jullian <benjamin.jullian@zenika.com>
2025-10-02 12:26:31 +0200

    refactor: enhance logDeepObject function to conditionally use colors based on environment

--- src/functions/log-deep-object.ts
+++ src/functions/log-deep-object.ts
@@ -1,3 +1,9 @@
 import { inspect } from 'node:util';
 
-export const logDeepObject = (object: unknown): void => console.log(inspect(object, { depth: null, colors: true }));
+export const logDeepObject = (object: unknown): void => {
+  const env = process.env.NODE_ENV;
+
+  const useColors = env !== 'test';
+
+  console.log(inspect(object, { depth: null, colors: useColors }));
+};
