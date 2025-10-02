def6388f420db8415a54aa46268713beb1767692
Benjamin Jullian <benjamin.jullian@zenika.com>
2025-10-02 12:17:45 +0200

    feat: implement main function to parse CLI parameters and log filtered or counted data

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import { data } from './data/data';
+import { countPeopleAndAnimals } from './functions/counting/count-people-and-animals';
+import { filterByAnimals } from './functions/filtering/filter-by-animals';
 import { logDeepObject } from './functions/log-deep-object';
+import { parseCLIParams } from './functions/parse-cli-params';
 
 function main(): void {
-  logDeepObject(data);
+  const params = parseCLIParams();
+
+  if (params.has('filter')) {
+    const filter = params.get('filter');
+    logDeepObject(filterByAnimals(data, filter));
+  } else if (params.has('count')) {
+    logDeepObject(countPeopleAndAnimals(data));
+  }
 }
 
 main();
