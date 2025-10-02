c8f9569d6ff029a03c5f5f1c2c6fd04ff0699852
Benjamin Jullian <benjamin.jullian@zenika.com>
2025-10-02 12:43:41 +0200

    feat: add filter case to E2E tests with expected results

--- tests/e2e/app.e2e.test.ts
+++ tests/e2e/app.e2e.test.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'node:child_process';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { COUNT_RESULT } from './results/count.result';
+import { FILTER_RESULT } from './results/filter.result';
 
 describe('E2E Application Tests', () => {
   beforeEach(() => {
@@ -15,5 +16,15 @@ describe('E2E Application Tests', () => {
     const output = result.toString();
 
     expect(output).toEqual(COUNT_RESULT);
+  });
+
+  it('should pass filter case', () => {
+    const result = execSync('node dist/app.js --filter=ry', {
+      stdio: ['inherit'],
+    });
+
+    const output = result.toString();
+
+    expect(output).toEqual(FILTER_RESULT);
   });
 });
---
+++ tests/e2e/results/filter.result.ts
@@ -0,0 +1,15 @@
+export const FILTER_RESULT = `[
+  {
+    name: 'Uzuzozne',
+    people: [
+      { name: 'Lillie Abbott', animals: [ { name: 'John Dory' } ] }
+    ]
+  },
+  {
+    name: 'Satanwi',
+    people: [
+      { name: 'Anthony Bruno', animals: [ { name: 'Oryx' } ] }
+    ]
+  }
+]
+`;
